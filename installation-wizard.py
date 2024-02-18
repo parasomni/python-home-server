0a4a8caf7f64a56997dd3ec68b798b392402142a
rysecx <lacree.legacy@proton.me>
2024-02-18 18:11:27 +0100

    added version 1.0.2

--- installation-wizard.py
+++ installation-wizard.py
@@ -62,6 +62,8 @@ def install_server():
     global client4
     global alert_email
     global trash_email
+    global time_delay
+    global max_number_conn_ddos
 
     check_dir(ultron_path)
     
@@ -106,22 +108,33 @@ def install_server():
             else:
                 update_text(f"no token found. skipping.")
                 pass
+            update_text(f'setting time delay to {time_delay}s')
             config = f"""
-            # server configuration
+            ### server configuration ###
             
+            ## client assets
             # Indicates the client paths
                     ,{client1},
                     ,{client2},
                     ,{client3},
                     ,{client3},
             
+            
+            ## server assets
             # Indicates the private key
                     ,/etc/ultron-server/prvt.key,
             
             # Indicates the vaild token file path
                     ,/etc/ultron-server/valid-tokens.txt,
             
-            # Denial of Service protection
+            # Server time delay
+                    ,{time_delay},
+                    
+            
+            ## Denial of Service protection
+            # maximum number of connections within 10 seconds to initialize server shutdown 
+                    ,{max_number_conn_ddos},
+            
             # Email address which should report upcoming alerts
                     ,{trash_email},
             
@@ -139,7 +152,7 @@ def install_server():
             update_text(config)
             update_text('setting up triggers')
             try:
-                os.system('cp us-v1.1.5-stable.py /usr/bin/us')
+                os.system('cp us-v1.1.6-stable.py /usr/bin/us')
                 os.system('chmod +x /usr/bin/us')
                 update_text("successfully installed ultron-server!")
             except Excpetion as e:
@@ -149,6 +162,8 @@ def install_server():
         client2 = ent_client2.get()
         client3 = ent_client3.get()
         client4 = ent_client4.get()
+        time_delay = ent_time_delay.get()
+        max_number_conn_ddos = ent_max_ddos.get()
         alert_email = ent_alert_email.get()
         trash_email = ent_trash_email.get()
         cred_file = txt_edit.get("1.0", tk.END)
@@ -160,7 +175,7 @@ def install_server():
         root = tk.Tk()
         root.title("Server setup")
         
-        text_output = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=100, height=15, state=tk.DISABLED)
+        text_output = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=100, height=20, state=tk.DISABLED)
         text_output.grid(row=0, column=0, columnspan=2)
         
         start_button = tk.Button(root, text="Install", command=lambda: threading.Thread(target=server_setup).start())
@@ -183,58 +198,77 @@ def install_server():
     root = tk.Tk()
     root.title("Server setup")
     root.resizable(width=False, height=False)
+    
     # general information
     lbl_info = tk.Label(root, text="Please fill in the gaps with the required information.")
     lbl_info.grid(row=0, column=0, sticky=tk.N)    
+    
     # Client setup
     frm_clients = tk.Frame(root, bd=5)
+    frm_clients.grid(row=1, column=0, sticky=tk.W)
     lbl_clients = tk.Label(frm_clients, text="Please specify the filesystem paths each client has access to:", fg="orange")
-    lbl_clients.grid(row=1, column=0, sticky=tk.E)
+    lbl_clients.grid(row=1, column=0, sticky=tk.W) 
+    lbl_clients.grid(row=1, column=0, sticky=tk.W)
     lbl_client1 = tk.Label(frm_clients, text="Client 1: ")
     lbl_client1.grid(row=2, column=0, sticky=tk.E)
     ent_client1 = tk.Entry(frm_clients, width=50)
-    ent_client1.grid(row=2, column=1, sticky=tk.E)
+    ent_client1.grid(row=2, column=1, sticky=tk.W)
     lbl_client2 = tk.Label(frm_clients, text="Client 2: ")
     lbl_client2.grid(row=3, column=0, sticky=tk.E)
     ent_client2 = tk.Entry(frm_clients, width=50)
-    ent_client2.grid(row=3, column=1, sticky=tk.E)    
+    ent_client2.grid(row=3, column=1, sticky=tk.W)    
     lbl_client3 = tk.Label(frm_clients, text="Client 3: ")
     lbl_client3.grid(row=4, column=0, sticky=tk.E)
     ent_client3 = tk.Entry(frm_clients, width=50)
-    ent_client3.grid(row=4, column=1, sticky=tk.E)    
+    ent_client3.grid(row=4, column=1, sticky=tk.W)    
     lbl_client4 = tk.Label(frm_clients, text="Client 4: ")
     lbl_client4.grid(row=5, column=0, sticky=tk.E)
     ent_client4 = tk.Entry(frm_clients, width=50)
-    ent_client4.grid(row=5, column=1, sticky=tk.E)     
-    lbl_clients.grid(row=1, column=0, sticky="e") 
-    frm_clients.grid(row=1, column=0, sticky="ew")
+    ent_client4.grid(row=5, column=1, sticky=tk.W)     
+    
+    # server time delay setup
+    frm_td = tk.Frame(root, bd=1)
+    frm_td.grid(row=6, column=0, sticky=tk.W)    
+    lbl_td = tk.Label(frm_td, text=" Server time delay setup: ", fg="orange")
+    lbl_td.grid(row=6, column=0, sticky=tk.W)
+    ent_time_delay = tk.Entry(frm_td, width=20)
+    ent_time_delay.grid(row=7, column=1, sticky=tk.N)
+    lbl_time_delay = tk.Label(frm_td, text=" Please enter the server time delay in seconds: ")
+    lbl_time_delay.grid(row=7, column=0, sticky=tk.N)
+    
     # DoS setup
-    frm_dos = tk.Frame(root, bd=3)
-    lbl_dos = tk.Label(frm_dos, text="Denial of service protection setup: ", fg="orange")
+    frm_dos = tk.Frame(root, bd=4)
+    frm_dos.grid(row=8, column=0, sticky=tk.W)
+    lbl_dos = tk.Label(frm_dos, text="Denial of Service protection setup: ", fg="orange")
+    lbl_dos.grid(row=8, column=0, sticky=tk.W)
+    ent_max_ddos = tk.Entry (frm_dos, width=15)
+    ent_max_ddos.grid(row=9, column=1, sticky=tk.W)
     ent_alert_email = tk.Entry(frm_dos, width=50)
-    ent_alert_email.grid(row=7, column=1, sticky=tk.E)
+    ent_alert_email.grid(row=10, column=1, sticky=tk.E)
     ent_trash_email = tk.Entry(frm_dos, width= 50)
-    ent_trash_email.grid(row=8, column=1, sticky=tk.E)    
+    ent_trash_email.grid(row=11, column=1, sticky=tk.E) 
+    lbl_max_ddos = tk.Label(frm_dos, text="Please enter the maximum number of connections within 10s to initialize server shutdown: ")
+    lbl_max_ddos.grid(row=9, column=0, sticky=tk.W)
     lbl_alert_email = tk.Label(frm_dos, text="Please enter the email address to which upcoming alerts should be reported: ")
-    lbl_alert_email.grid(row=7, column=0, sticky=tk.E)    
+    lbl_alert_email.grid(row=10, column=0, sticky=tk.W)    
     lbl_trash_email = tk.Label(frm_dos, text="Please enter the email address which should report upcoming alerts: ")
-    lbl_trash_email.grid(row=8, column=0, sticky=tk.E)
-    lbl_dos.grid(row=6, column=0, sticky="w")
+    lbl_trash_email.grid(row=11, column=0, sticky=tk.W)
+
     # credential file
     txt_edit = tk.Text(frm_dos, width=50, height=1)
-    txt_edit.grid(row=9, column=1, sticky="")
-    lbl_credit_file = tk.Label(frm_dos, text="Now please specify the credential file which includes the login password of your report email address: ")
-    lbl_credit_file.grid(row=9, column=0, sticky=tk.E)   
+    txt_edit.grid(row=12, column=1, sticky="")
+    lbl_credit_file = tk.Label(frm_dos, text="Please select the credential file with the login password of your report email address: ")
+    lbl_credit_file.grid(row=12, column=0, sticky=tk.W)   
     btn_fetch_file = tk.Button(frm_dos, relief=tk.RAISED, text=".", command=fetch_file)
-    btn_fetch_file.grid(row=9, column=2, sticky="ew", padx=5, pady=5)
-    frm_dos.grid(row=6, column=0, sticky="e")
+    btn_fetch_file.grid(row=12, column=2, sticky="ew", padx=5, pady=5)
+    
     # Main buttons
     frm_buttons = tk.Frame(root, relief=tk.RAISED, bd=2)
     btn_submit = tk.Button(frm_buttons, text="Submit", command=complete_setup)
-    btn_submit.grid(row=10, column=2, sticky=tk.E, padx=350, pady=5)    
+    btn_submit.grid(row=13, column=2, sticky=tk.E, padx=230, pady=5)    
     btn_exit = tk.Button(frm_buttons, text="Exit", command=root.destroy)
-    btn_exit.grid(row=10, column=0, sticky=tk.W, padx=200, pady=5)    
-    frm_buttons.grid(row=10, column=0, sticky="ewn")
+    btn_exit.grid(row=13, column=0, sticky=tk.W, padx=200, pady=5)    
+    frm_buttons.grid(row=13, column=0, sticky="ewn")
     root.mainloop()  
 
 def install_client():
@@ -372,7 +406,7 @@ Please be aware that there is no possibility to restore any files after proceedi
         window.mainloop()        
 
 def start_wizard():
-    info = """Installation Wizard version 1.0.1
+    info = """Installation Wizard version 1.0.2
     
 Welcome to the guided installation process for the file-sharing server named ultron-server. 
 Kindly proceed by selecting your desired option.."""
